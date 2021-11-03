edab4d9021666bb90ef0403a8a334603c1a96c4d
wilsonchristianhalim <91448645+wilsonchristianhalim@users.noreply.github.com>
2021-11-03 18:47:48 +0700

    Main Menu

---
+++ src/View/MainMenu.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+import javax.swing.*;
+/**
+ *
+ * @author Wilson
+ */
+public class MainMenu {
+    
+    public static void main(String[] args) {
+        int menu;
+        menu=Integer.parseInt(JOptionPane.showInputDialog(null, "Main Menu"
+                + "\n1. Divisi Game"
+                + "\n2. Match"
+                + "\n3. News"
+                + "\n4. Company"
+                + "\n5. Form"));
+        switch (menu){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+    }
+}
