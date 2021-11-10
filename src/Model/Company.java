cde6e8b7102b408a567699c1acf9a5aa77cc52d5
wilsonchristianhalim <91448645+wilsonchristianhalim@users.noreply.github.com>
2021-11-10 12:01:13 +0700

    company

--- src/Model/Company.java
+++ src/Model/Company.java
@@ -4,11 +4,62 @@
  * and open the template in the editor.
  */
 package Model;
+<<<<<<< Updated upstream
 
+=======
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+>>>>>>> Stashed changes
 /**
  *
  * @author Wilson
  */
 public class Company {
+<<<<<<< Updated upstream
     
+=======
+    private ArrayList <Contact> contact = new ArrayList<>();
+    private ArrayList <Partner> partner = new ArrayList<>();
+
+    public Company() {
+    }
+
+    public ArrayList<Contact> getContact() {
+        return contact;
+    }
+
+    public void setContact(ArrayList<Contact> contact) {
+        this.contact = contact;
+    }
+
+    public ArrayList<Partner> getPartner() {
+        return partner;
+    }
+
+    public void setPartner(ArrayList<Partner> partner) {
+        this.partner = partner;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" + "contact=" + contact + ", partner=" + partner + '}';
+    }
+    
+    public void AddContact(String id_contact, String contact_name){
+        
+    }
+    
+    public void DeleteContact(String contact_name){
+        boolean found = false;
+        for (int i = 0; i < contact.size(); i++) {
+            if (contact.get(i).getContact_Name().equals(contact_name)) {
+                found=true;
+                contact.remove(i);
+            }
+        }
+        if (!found) {
+            JOptionPane.showMessageDialog(null, "Contact Tidak Ditemukan");
+        }
+    }
+>>>>>>> Stashed changes
 }
---
+++ src/Model/Contact.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Wilson
+ */
+public class Contact extends Company{
+    private String ID_Contact;
+    private String Contact_Name;
+
+    public Contact(String ID_Contact, String Contacr_Name) {
+        this.ID_Contact = ID_Contact;
+        this.Contact_Name = Contact_Name;
+    }
+
+    public String getID_Contact() {
+        return ID_Contact;
+    }
+
+    public void setID_Contact(String ID_Contact) {
+        this.ID_Contact = ID_Contact;
+    }
+
+    public String getContact_Name() {
+        return Contact_Name;
+    }
+
+    public void setContact_Name(String Contact_Name) {
+        this.Contact_Name = Contact_Name;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" + "ID_Contact=" + ID_Contact + ", Contact_Name=" + Contact_Name + '}';
+    }
+    
+
+}
