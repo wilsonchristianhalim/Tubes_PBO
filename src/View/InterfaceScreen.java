30b13af669c11fa8c265017bf52f0370aba49f7d
vitovandersen <vitovandersen26@gmail.com>
2021-11-18 13:13:06 +0700

    InterfaceScreen
    Ini digunakan untuk  membuat screen yang value nya konstan seperti panjang lebar dari windows

---
+++ src/View/InterfaceScreen.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+/**
+ *
+ * @author Vito
+ */
+public interface InterfaceScreen {
+    public final int WINDOWS_WIDTH = 800 ;
+    public final int WINDOWS_HEIGHT = 600  ;
+}
