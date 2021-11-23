14742c69dcc33e8b333d26a1a1a0e084f049b311
vitovandersen <vitovandersen26@gmail.com>
2021-11-23 19:02:37 +0700

    Update Match.java

--- src/Model/Match.java
+++ src/Model/Match.java
@@ -10,54 +10,62 @@ package Model;
  * @author Wilson
  */
 public class Match {
-    private String ID_Match;
-    private String Match_Name;
-    private String Date_Match;
-    private String Result;
+    private String id_Match;
+    private String match_Name;
+    private String date_Match;
+    private String result;
 
-    public Match(String ID_Match, String Match_Name, String Date_Match, String Result) {
-        this.ID_Match = ID_Match;
-        this.Match_Name = Match_Name;
-        this.Date_Match = Date_Match;
-        this.Result = Result;
+    public Match(String id_Match, String match_Name, String date_Match, String result) {
+        this.id_Match = id_Match;
+        this.match_Name = match_Name;
+        this.date_Match = date_Match;
+        this.result = result;
     }
 
-    public String getID_Match() {
-        return ID_Match;
+    public String getId_Match() {
+        return id_Match;
     }
 
-    public void setID_Match(String ID_Match) {
-        this.ID_Match = ID_Match;
+    public void setId_Match(String id_Match) {
+        this.id_Match = id_Match;
     }
 
     public String getMatch_Name() {
-        return Match_Name;
+        return match_Name;
     }
 
-    public void setMatch_Name(String Match_Name) {
-        this.Match_Name = Match_Name;
+    public void setMatch_Name(String match_Name) {
+        this.match_Name = match_Name;
     }
 
     public String getDate_Match() {
-        return Date_Match;
+        return date_Match;
     }
 
-    public void setDate_Match(String Date_Match) {
-        this.Date_Match = Date_Match;
+    public void setDate_Match(String date_Match) {
+        this.date_Match = date_Match;
     }
 
     public String getResult() {
-        return Result;
+        return result;
     }
 
-    public void setResult(String Result) {
-        this.Result = Result;
+    public void setResult(String result) {
+        this.result = result;
     }
 
     @Override
     public String toString() {
-        return "Match{" + "ID_Match=" + ID_Match + ", Match_Name=" + Match_Name + ", Date_Match=" + Date_Match + ", Result=" + Result + '}';
+        StringBuilder sb = new StringBuilder();
+        sb.append("Match{iD_Match=").append(id_Match);
+        sb.append(", match_Name=").append(match_Name);
+        sb.append(", date_Match=").append(date_Match);
+        sb.append(", result=").append(result);
+        sb.append('}');
+        return sb.toString();
     }
+
+    
     
     
 }
