3e1c0ea108f5de98b6e45ff80ed88c4080dea8d5
wilsonchristianhalim <91448645+wilsonchristianhalim@users.noreply.github.com>
2021-11-25 01:32:10 +0700

    a

---
+++ src/View/ShowTitleNews.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+import javax.swing.*;
+/**
+ *
+ * @author Wilson
+ */
+public class ShowTitleNews {
+    private JComboBox titleNews;
+    private JButton submit, back;
+    
+    public void PrintNews(){
+        JFrame news = new JFrame("News")
+    }
+}
