4c25ae6883b2fb492fb8491db057522af697e5fb
Panji <68853735+panjiandika1011@users.noreply.github.com>
2021-11-25 12:40:27 +0700

    Update AddPlayer.java

--- src/View/AddPlayer.java
+++ src/View/AddPlayer.java
@@ -9,26 +9,12 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class AddPlayer implements ActionListener{
-    private JLabel ID_Player, ;
-    private JTextField isiID, isiNama;
+    private JLabel username, nama, umur, tanggalLahir ;
+    private JTextField isiUname, isiNama, isiUmur, isiTanggalLahir;
     private JButton add, back;
     
-    public AddPlayer(){
-        layoutAddPlayer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        layoutAddPlayer.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        layoutAddPlayer.getContentPane().setBackground(StyleSheet.backgroundColor);
+    public void AddPlayer(){
         
-        title = new JLabel("~ A d d  P l a y e r ~",JLabel.CENTER);
-        title.setBounds(0, 0, (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight()/5);
-        title.setFont(StyleSheet.titleFont);
-        
-        buttonPanel = new JPanel();
-        buttonPanel.setLayout(null);
-        
-        add = new JButton("Add Player");
-        add.setBounds(600, 50, 200, 70);
-        add.addActionListener(this);
-        add.setFont(StyleSheet.buttonFont);
         
         
     }
